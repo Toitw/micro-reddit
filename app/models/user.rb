730cd92e640305d036592d8f16c63ef8678b9495
juroga_new <juanjrgast@gmail.com>
2023-05-14 12:14:09 +0200

    Created the post models, references to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, :email, :nickname, presence: true
+    validates :email, :nickname, uniqueness: true
+    validates :name, :nickname, length: {in: 3..20}
+
+    has_many :posts
 end
