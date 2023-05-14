d824ab83d9705e1a671b1e0ed958b5071a8d5432
juroga_new <juanjrgast@gmail.com>
2023-05-14 12:16:40 +0200

    Add validations to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :user
+    validates :title, :body, presence: true
+    validates :title, length: {in: 3..20}
+    validates :body, length: {maximum: 1000}
+
+    belongs_to :user
+    has_many :comments
 end
